2a32765d7087494aafb6b36bbb2ff86f3afe4da2
DanieleSalinitro <daniele.salinitro@icloud.com>
2024-04-17 19:26:48 +0200

    modifiche

--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
 import {reactive} from 'vue'; 
 export const store = reactive({
     apiUrl: 'https://api.themoviedb.org/3/', 
-    endPoint : {
-        movies: 'search/movie',
+    endPoint: {
+        movie: 'search/movie',
         tv: 'search/tv'
     }, 
-    apiKey: 'f44e950b1df9ad1447febc19b512559c'
+    options: {
+        params: {
+            api_key: 'f44e950b1df9ad1447febc19b512559c',
+            query: 'a',
+        }
+    }
 })
